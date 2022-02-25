fe2ebd15830e982d3d53cbf3b984a55b576bd218
Luke Pratley <Luke.Pratley@gmail.com>
2022-02-24 21:44:25 -0500

    added ability to pass different metrics for training

--- building_road_segmentation/optimization_factory.py
+++ building_road_segmentation/optimization_factory.py
@@ -9,16 +9,13 @@ from tensorflow.keras.utils import Progbar
 class Trainer():
 
     def __init__(self, model, loss_fn, optimizer, train_acc_metric,
-                 val_acc_metric):
+                 val_acc_metric, train_metrics = []):
         self.model = model
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.train_acc_metric = train_acc_metric
         self.val_acc_metric = val_acc_metric
-        self.train_iou_metric = tf.keras.metrics.MeanIoU(
-                self.model.num_classes)
-        self.test_iou_metric = tf.keras.metrics.MeanIoU(
-                self.model.num_classes)
+        self.train_metrics = train_metrics
 
     @tf.function
     def train_step(self, x, y):
@@ -29,7 +26,8 @@ class Trainer():
         self.optimizer.apply_gradients(zip(grads,
                                            self.model.trainable_weights))
         self.train_acc_metric.update_state(y, result)
-        self.train_iou_metric.update_state(y, result)
+        for t in self.train_metrics:
+            t[1].update_state(y, result)
         return loss_value
 
     @tf.function
@@ -43,51 +41,51 @@ class Trainer():
             epochs,
             _callbacks=None,
             interval=0.5):
-           logs = {}
-           callbacks = tf.keras.callbacks.CallbackList(_callbacks,
-                                                      add_history=True,
-                                                      model=self.model)
-           callbacks.on_train_begin(logs=logs)
-           for epoch in range(epochs):
-                print(f'epoch: {epoch}')
-                callbacks.on_epoch_begin(epoch, logs=logs)
-                pb_i = Progbar(len(train_dataset.x),
-                               interval=interval,
-                               unit_name="step")
+        logs = {}
+        callbacks = tf.keras.callbacks.CallbackList(_callbacks,
+                                                    add_history=True,
+                                                    model=self.model)
+        callbacks.on_train_begin(logs=logs)
+        for epoch in range(epochs):
+            print(f'epoch: {epoch}')
+            callbacks.on_epoch_begin(epoch, logs=logs)
+            pb_i = Progbar(len(train_dataset.x),
+                           interval=interval,
+                           unit_name="step")
 
-                for step, (x_batch_train,
-                           y_batch_train) in enumerate(train_dataset):
+            for step, (x_batch_train,
+                       y_batch_train) in enumerate(train_dataset):
 
-                    self.model.reset_states()
-                    callbacks.on_batch_begin(step, logs=logs)
-                    callbacks.on_train_batch_begin(step, logs=logs)
+                self.model.reset_states()
+                callbacks.on_batch_begin(step, logs=logs)
+                callbacks.on_train_batch_begin(step, logs=logs)
 
-                    loss_value = self.train_step(x_batch_train, y_batch_train)
+                loss_value = self.train_step(x_batch_train, y_batch_train)
 
-                    callbacks.on_train_batch_end(step, logs=logs)
-                    callbacks.on_batch_end(step, logs=logs)
-                    train_acc = self.train_acc_metric.result()
+                callbacks.on_train_batch_end(step, logs=logs)
+                callbacks.on_batch_end(step, logs=logs)
+                train_acc = self.train_acc_metric.result()
 
-                    loss_val = loss_value
-                    acc_val = train_acc
-                    iou_val = self.train_iou_metric.result()
-                    pb_i.add(train_dataset.batch_size,
-                             values=[('loss', loss_val), ('acc', acc_val), ('iou', iou_val)])
+                loss_val = loss_value
+                acc_val = train_acc
+                pb_i.add(train_dataset.batch_size,
+                         values=[('loss', loss_val), ('acc', acc_val)] +
+                         [(t[0], t[1].result()) for t in self.train_metrics])
 
-                # Reset training metrics at the end of each epoch
-                self.train_acc_metric.reset_states()
-                if val_dataset != None:
-                    # Run a validation loop at the end of each epoch.
-                    for x_batch_val, y_batch_val in val_dataset:
-                        self.test_step(x_batch_val, y_batch_val)
+            # Reset training metrics at the end of each epoch
+            self.train_acc_metric.reset_states()
+            if val_dataset != None:
+                # Run a validation loop at the end of each epoch.
+                for x_batch_val, y_batch_val in val_dataset:
+                    self.test_step(x_batch_val, y_batch_val)
 
-                    val_acc = self.val_acc_metric.result()
-                    self.val_acc_metric.reset_states()
-                callbacks.on_epoch_end(epoch, logs=logs)
+                val_acc = self.val_acc_metric.result()
+                self.val_acc_metric.reset_states()
+            callbacks.on_epoch_end(epoch, logs=logs)
 
-            callbacks.on_train_end(logs=logs)
-            history_object = None
-            for cb in callbacks:
-                if isinstance(cb, tf.keras.callbacks.History):
-                    history_object = cb
-            return history_object
+        callbacks.on_train_end(logs=logs)
+        history_object = None
+        for cb in callbacks:
+            if isinstance(cb, tf.keras.callbacks.History):
+                history_object = cb
+        return history_object
