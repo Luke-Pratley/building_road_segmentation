e6170affa10ac9abfbeba61777871667d55efcfd
Luke Pratley <Luke.Pratley@gmail.com>
2022-02-25 10:11:26 -0500

    renamed class and moved function

--- building_road_segmentation/data_generator.py
+++ building_road_segmentation/data_generator.py
@@ -10,7 +10,7 @@ import PIL
 import tensorflow.image
 
 
-class read_data(tf.keras.utils.Sequence):
+class READ_DATA(tf.keras.utils.Sequence):
 
     def __init__(self, x_set, y_set, batch_size):
         self.x, self.y = x_set, y_set
@@ -30,7 +30,7 @@ class read_data(tf.keras.utils.Sequence):
             ])
 
 
-class read_and_augment_data(tf.keras.utils.Sequence):
+class READ_AND_AUGMENT_DATA(tf.keras.utils.Sequence):
 
     class Augment(Enum):
         ORIGINAL = 1
@@ -50,6 +50,19 @@ class read_and_augment_data(tf.keras.utils.Sequence):
     def __len__(self):
         return math.ceil(len(self.x) / self.batch_size)
 
+    def apply_augment(image, aug):
+        if aug is self.Augment.ORIGINAL:
+            return image
+        if aug is self.Augment.LRFLIP:
+            return tf.image.flip_left_right(image)
+        if aug is self.Augment.UDFLIP:
+            return tf.image.flip_up_down(image)
+        if aug is self.Augment.ROT90:
+            return tf.image.rot90(image)
+        if aug is self.Augment.ROT270:
+            return tf.image.rot90(image, k=3)
+
+
     def __getitem__(self, idx):
         batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
@@ -63,18 +76,6 @@ class read_and_augment_data(tf.keras.utils.Sequence):
                                 ynp.load(file_name[0]).astype(np.float32),
                                 file_name[1]) for file_name in batch_y
                         ])
-
-    def apply_augment(image, aug):
-        if aug is self.Augment.ORIGINAL:
-            return image
-        if aug is self.Augment.LRFLIP:
-            return tf.image.flip_left_right(image)
-        if aug is self.Augment.UDFLIP:
-            return tf.image.flip_up_down(image)
-        if aug is self.Augment.ROT90:
-            return tf.image.rot90(image)
-        if aug is self.Augment.ROT270:
-            return tf.image.rot90(image, k=3)
 
 
 class test_data(tf.keras.utils.Sequence):
