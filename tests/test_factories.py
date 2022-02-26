ac17cd4efe48ca43c980e26f7a6f3ff41d6e645e
Luke Pratley <Luke.Pratley@gmail.com>
2022-02-26 16:39:40 -0500

    added attention gate test and added new test file

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -11,75 +11,10 @@ import building_road_segmentation.data_generator as data_generator
 import numpy as np
 
 
-def test_conv_block():
-
-    block = unet_factory.ConvBlock(
-        number_of_start_kernels=8,
-        kernel_shape=(3, 3),
-        activation=tf.nn.relu,
-        kernel_initializer=tf.keras.initializers.he_normal())
-    assert isinstance(block, tf.keras.Model)
-    inp = tf.constant(np.random.normal(0, 1, (4, 16, 16, 3)), dtype=np.float32)
-    out = block(inp)
-    assert (out.shape == (4, 16, 16, 8))
-    assert (out.shape.as_list() == [4, 16, 16, 8])
-    blocks = block.layers
-    assert len(blocks) == 6
-    assert isinstance(blocks[0], tf.keras.layers.Conv2D)
-    assert isinstance(blocks[1], tf.keras.layers.BatchNormalization)
-    assert isinstance(blocks[2], tf.keras.layers.Activation)
-    assert isinstance(blocks[3], tf.keras.layers.Conv2D)
-    assert isinstance(blocks[4], tf.keras.layers.BatchNormalization)
-    assert isinstance(blocks[5], tf.keras.layers.Activation)
-
-
-def test_downlayer():
-
-    x = unet_factory.DownLayer(
-        number_of_start_kernels=8,
-        kernel_shape=(3, 3),
-        activation=tf.nn.relu,
-        pooling_amount=2,
-        dropout_rate=0.5,
-        kernel_initializer=tf.keras.initializers.he_normal())
-    assert isinstance(x, tf.keras.Model)
-    inp = tf.constant(np.random.normal(0, 1, (4, 16, 16, 3)), dtype=np.float32)
-    out = x(inp)
-    assert (out.shape.as_list() == [4, 8, 8, 8])
-    blocks = x.layers
-    assert len(blocks) == 3
-    assert isinstance(blocks[0], tf.keras.layers.MaxPooling2D)
-    assert isinstance(blocks[1], tf.keras.layers.Dropout)
-    assert isinstance(blocks[2], unet_factory.ConvBlock)
-
-
-def test_uplayer():
-
-    x = unet_factory.UpLayer(
-        number_of_start_kernels=8,
-        kernel_shape=(3, 3),
-        activation=tf.nn.relu,
-        pooling_amount=2,
-        dropout_rate=0.5,
-        kernel_initializer=tf.keras.initializers.he_normal())
-    assert isinstance(x, tf.keras.Model)
-    inp1 = tf.constant(np.random.normal(0, 1, (4, 8, 8, 3)), dtype=np.float32)
-    inp2 = tf.constant(np.random.normal(0, 1, (4, 16, 16, 3)),
-                       dtype=np.float32)
-    out = x([inp1, inp2])
-    assert (out.shape.as_list() == [4, 16, 16, 8])
-    blocks = x.layers
-    assert len(blocks) == 4
-    assert isinstance(blocks[0], tf.keras.layers.UpSampling2D)
-    assert isinstance(blocks[1], tf.keras.layers.Concatenate)
-    assert isinstance(blocks[2], tf.keras.layers.Dropout)
-    assert isinstance(blocks[3], unet_factory.ConvBlock)
-
-
 def test_basic_unet():
     unet_levels = 6
     number_of_categories = 1
-    x = unet_factory.BasicUnet(
+    x = unet_factory.BasicUNet(
         number_of_categories=number_of_categories,
         unet_levels=unet_levels,
         number_of_start_kernels=4,
@@ -106,9 +41,9 @@ def test_basic_unet():
     optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
     loss_fn = tf.keras.losses.BinaryCrossentropy()
     x.compile(optimizer=optimizer, loss=loss_fn)
-    data_gen = data_generator.TEST_DATA(np.random.uniform(0, 1, (16, 128, 128, 3)),
-                                        np.random.uniform(0, 1, (16, 128, 128, 1)),
-                                        4)
+    data_gen = data_generator.TEST_DATA(
+        np.random.uniform(0, 1, (16, 128, 128, 3)),
+        np.random.uniform(0, 1, (16, 128, 128, 1)), 4)
     x.fit(data_gen, epochs=2)
 
 
