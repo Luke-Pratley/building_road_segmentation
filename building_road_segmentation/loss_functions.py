b199dd799cdbbdc47e0d72acd05330ba4ba9fcd2
Luke Pratley <Luke.Pratley@gmail.com>
2022-02-28 15:29:12 -0500

    added weighted cross entropy and dice losses

---
+++ building_road_segmentation/loss_functions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import tensorflow.keras
+
+
+def weighted_dice_loss(weights):
+
+    def dice_loss(y_true, y_pred):
+        tfweights = tf.constant(weights, dtype=y_pred.dtype)
+        if not tf.is_tensor(y_pred): y_pred = tf.constant(y_pred)
+        y_true = tf.cast(y_true, y_pred.dtype)
+        return 1 - 2 * (tf.reduce_sum(
+            tf.math.multiply(tf.math.multiply(
+                tfweights, y_true), y_pred), axis=-1) + 1e-13) / (tf.reduce_sum(
+                    tf.multiply(y_true + y_pred, tfweights), axis=-1) + 1e-13)
+
+    return dice_loss
+
+
+def weighted_categorical_crossentropy(weights):
+
+    def cateogrical_cross_entropy(y_true, y_pred):
+        tfweights = tf.constant(weights, dtype=y_pred.dtype)
+        if not tf.is_tensor(y_pred): y_pred = tf.constant(y_pred)
+        y_true = tf.cast(y_true, y_pred.dtype)
+        return tf.keras.backend.categorical_crossentropy(
+            y_true * tfweights, y_pred)
+
+    return cateogrical_cross_entropy
---
+++ tests/test_losses.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import tensorflow as tf
+import tensorflow.keras
+
+import numpy as np
+import building_road_segmentation.loss_functions as loss_functions
+
+
+def test_constant_weighted_categorical_cross_entropy():
+    np.random.seed(0)
+    class_num = 5
+    test_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = true_input / np.sum(true_input, axis=-1)[:, :, :, np.newaxis]
+    test_input = test_input / np.sum(test_input, axis=-1)[:, :, :, np.newaxis]
+    weights = np.ones_like(test_input)
+    output = -np.sum(np.log(test_input) * true_input * weights, axis=(-1))
+
+    loss_fn = loss_functions.weighted_categorical_crossentropy(weights)
+
+    test_output = loss_fn(true_input, test_input).numpy()
+    assert test_output.shape == output.shape
+    assert np.allclose(output, test_output, rtol=1e-6, atol=1e-6)
+
+
+def test_constant_weighted_dice_loss():
+    np.random.seed(0)
+    class_num = 5
+    test_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = true_input / np.sum(true_input, axis=-1)[:, :, :, np.newaxis]
+    test_input = test_input / np.sum(test_input, axis=-1)[:, :, :, np.newaxis]
+    weights = np.ones_like(test_input)
+
+    output = 1 - 2 * (np.sum(
+        test_input * true_input * weights, axis=(-1)) + 1e-13) / (np.sum(
+            (test_input + true_input) * weights, axis=(-1)) + 1e-13)
+
+    loss_fn = loss_functions.weighted_dice_loss(weights)
+
+    test_output = loss_fn(true_input, test_input).numpy()
+    assert test_output.shape == output.shape
+    assert np.allclose(output, test_output, rtol=1e-6, atol=1e-6)
+
+
+def test_random_weighted_categorical_cross_entropy():
+    np.random.seed(0)
+    class_num = 5
+    test_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = true_input / np.sum(true_input, axis=-1)[:, :, :, np.newaxis]
+    test_input = test_input / np.sum(test_input, axis=-1)[:, :, :, np.newaxis]
+    weights = np.random.uniform(0, 1, size=test_input.shape)
+    output = -np.sum(np.log(test_input) * true_input * weights, axis=(-1))
+
+    loss_fn = loss_functions.weighted_categorical_crossentropy(weights)
+
+    test_output = loss_fn(true_input, test_input).numpy()
+    assert test_output.shape == output.shape
+    assert np.allclose(output, test_output, rtol=1e-6, atol=1e-6)
+
+def test_random_weighted_dice_loss():
+    np.random.seed(0)
+    class_num = 5
+    test_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = np.random.uniform(0, 1, size=(1, 128, 128, class_num))
+    true_input = true_input / np.sum(true_input, axis=-1)[:, :, :, np.newaxis]
+    test_input = test_input / np.sum(test_input, axis=-1)[:, :, :, np.newaxis]
+
+    weights = np.random.uniform(0, 1, size=test_input.shape)
+
+    output = 1 - 2 * (np.sum(
+        test_input * true_input * weights, axis=(-1)) + 1e-13) / (np.sum(
+            (test_input + true_input) * weights, axis=(-1)) + 1e-13)
+
+    loss_fn = loss_functions.weighted_dice_loss(weights)
+
+    test_output = loss_fn(true_input, test_input).numpy()
+    assert test_output.shape == output.shape
+    assert np.allclose(output, test_output, rtol=1e-6, atol=1e-6)
