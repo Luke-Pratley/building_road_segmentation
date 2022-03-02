5a5d6172c34d161880051d9e695fbcbae5b3dfa2
Luke Pratley <Luke.Pratley@gmail.com>
2022-03-01 19:15:46 -0500

    trying to fix building efficientnet model

--- building_road_segmentation/unet_factory.py
+++ building_road_segmentation/unet_factory.py
@@ -299,6 +299,8 @@ class EfficientNetUNet(tf.keras.Model):
             activation='softmax' if number_of_categories > 1 else 'sigmoid',
             padding='same',
             kernel_initializer=kernel_initializer)
+    def build(input_shape):
+        self.efficient_model.build(input_shape)
 
     def call(self, input_tensor, training=False):
         down_outputs = []
