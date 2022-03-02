150fd4db7b63dd744530a6c8f23c390f3189b782
Luke Pratley <Luke.Pratley@gmail.com>
2022-03-02 16:42:19 -0500

    example of binary dice loss

--- building_road_segmentation/loss_functions.py
+++ building_road_segmentation/loss_functions.py
@@ -12,9 +12,30 @@ def weighted_dice_loss(weights):
         if not tf.is_tensor(y_pred): y_pred = tf.constant(y_pred)
         y_true = tf.cast(y_true, y_pred.dtype)
         return 1 - 2 * (tf.reduce_sum(
-            tf.math.multiply(tf.math.multiply(
-                tfweights, y_true), y_pred), axis=-1) + 1e-13) / (tf.reduce_sum(
-                    tf.multiply(y_true + y_pred, tfweights), axis=-1) + 1e-13)
+            tf.math.multiply(tf.math.multiply(tfweights, y_true), y_pred),
+            axis=-1) + 1e-13) / (tf.reduce_sum(
+                tf.multiply(y_true + y_pred, tfweights), axis=-1) + 1e-13)
+
+    return dice_loss
+
+
+def weighted_binary_dice_loss(weights, not_weights):
+
+    def dice_loss(y_true, y_pred):
+        tfweights = tf.constant(weights, dtype=y_pred.dtype)
+        tfnot_weights = tf.constant(not_weights, dtype=y_pred.dtype)
+        if not tf.is_tensor(y_pred): y_pred = tf.constant(y_pred)
+        y_true = tf.cast(y_true, y_pred.dtype)
+        not_y_true = 1 - y_true
+        not_y_pred = 1 - y_pred
+        return 1 - 2 * (tf.reduce_sum(
+            tf.math.multiply(tf.math.multiply(tfweights, y_true), y_pred) +
+            tf.math.multiply(tf.math.multiply(tfnot_weights, not_y_true),
+                             not_y_pred),
+            axis=-1) + 1e-13) / (tf.reduce_sum(
+                tf.multiply(y_true + y_pred, tfweights) +
+                tf.multiply(not_y_true + not_y_pred, tfnot_weights),
+                axis=-1) + 1e-13)
 
     return dice_loss
 
