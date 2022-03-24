928198627e8e489afa1082ca3ecab737235bff11
Luke Pratley <Luke.Pratley@gmail.com>
2022-03-23 21:51:51 -0400

    added docstrings for unet_factory

--- building_road_segmentation/unet_factory.py
+++ building_road_segmentation/unet_factory.py
@@ -7,9 +7,23 @@ import tensorflow.keras.applications.efficientnet
 
 
 class ConvBlock(tf.keras.Model):
+    """
+    A key convolutional block for UNet: (conv2d -> batchnorm -> activation) x 2
+    """
 
     def __init__(self, number_of_start_kernels, kernel_shape, activation,
                  residual, kernel_initializer):
+        """
+        Initilizing the layers
+
+        Input:
+        number_of_start_kernels: The number of convolution kernels
+                                    to use in a convolutional layer
+        kernel_shape: The size of a kernel
+        activation: The name or object for the activation function.
+        residual: If to include a residual layer (for res-UNet) (bool)
+        kernel_initializer: How to initialize the kernel weights.
+        """
         super(ConvBlock, self).__init__(name='')
         self.residual = residual
         self.conv1 = tf.keras.layers.Conv2D(
@@ -36,6 +50,12 @@ class ConvBlock(tf.keras.Model):
                 kernel_initializer=kernel_initializer)
 
     def call(self, input_tensor, training=False):
+        """
+        A function that calls the layers
+        Inputs:
+        input_tensor: input to the first layer.
+        training: bool that is True if paramters are being trained.
+        """
         x = self.conv1(input_tensor)
         x = self.bn1(x, training)
         x = self.activation1(x)
@@ -50,9 +70,25 @@ class ConvBlock(tf.keras.Model):
 
 
 class DownLayer(tf.keras.Model):
+    """
+    A UNet layer within the encoder that pools/downsamples the input and applies a ConvBlock
+    """
 
     def __init__(self, number_of_start_kernels, kernel_shape, activation,
                  pooling_amount, dropout_rate, residual, kernel_initializer):
+        """
+        Initilizing the layers
+
+        Input:
+        number_of_start_kernels: The number of convolution kernels
+                                    to use in a convolutional layer
+        kernel_shape: The size of a kernel
+        activation: The name or object for the activation function.
+        pooling_amount: The factor to downsample/pool by.
+        dropout_rate: The dropout rate.
+        residual: If to include a residual layer (for res-UNet) (bool)
+        kernel_initializer: How to initialize the kernel weights.
+        """
         super(DownLayer, self).__init__(name='')
         self.pool = tf.keras.layers.MaxPooling2D(
             (pooling_amount, pooling_amount))
@@ -64,17 +100,41 @@ class DownLayer(tf.keras.Model):
                                    kernel_initializer=kernel_initializer)
 
     def call(self, input_tensor, training=False):
+        """
+        Calls the layers on the input, pooling -> dropout -> ConvBlock
+        Inputs:
+        input_tensor: input to the first layer.
+        training: bool that is True if paramters are being trained.
+
+        """
         x = self.pool(input_tensor)
         x = self.dropout(x, training)
         return self.convblock(x, training)
 
 
 class UpLayer(tf.keras.Model):
+    """
+    A UNet layer within the encoder that upsamples the input while
+    concatenating output from a DownLayer then applies
+    a dropout and a ConvBlock layer.
+    """
 
     def __init__(self, number_of_start_kernels, kernel_shape, activation,
                  pooling_amount, dropout_rate, residual, kernel_initializer):
+        """
+        Initilizing the layers
+
+        Input:
+        number_of_start_kernels: The number of convolution kernels
+                                    to use in a convolutional layer
+        kernel_shape: The size of a kernel
+        activation: The name or object for the activation function.
+        pooling_amount: The factor to downsample/pool by.
+        dropout_rate: The dropout rate.
+        residual: If to include a residual layer (for res-UNet) (bool)
+        kernel_initializer: How to initialize the kernel weights.
+        """
         super(UpLayer, self).__init__(name='')
-        # TODO: create option to switch between upsampling and transpose convolution
         self.upsample = tf.keras.layers.UpSampling2D(size=(pooling_amount,
                                                            pooling_amount))
         self.concat = tf.keras.layers.Concatenate(axis=-1)
@@ -86,6 +146,14 @@ class UpLayer(tf.keras.Model):
                                    kernel_initializer=kernel_initializer)
 
     def call(self, input_tensor, training=False):
+        """
+        Calls the layers on the input, pooling -> dropout -> ConvBlock
+        Inputs:
+        input_tensor: the input is actually two inputs
+        input[0] (layer to be upsampled) and input[1] (layer from DownLayer)
+        training: bool that is True if paramters are being trained.
+
+        """
         x = self.upsample(input_tensor[0])
         y = input_tensor[1]
         x = self.concat([x, y])
@@ -95,6 +163,12 @@ class UpLayer(tf.keras.Model):
 
 
 class BasicUNet(tf.keras.Model):
+    """
+    A UNet Convolutional Neural Network
+
+    The network will take an input image (values between 0 and 1) 
+    and perform semantic segmentation for a fixed number of classes.
+    """
 
     def __init__(self,
                  number_of_categories,
@@ -107,9 +181,28 @@ class BasicUNet(tf.keras.Model):
                  dropout_rate,
                  residual,
                  kernel_initializer=tf.keras.initializers.he_normal()):
+        """
+        Initilizing the layers
+
+        Input:
+        number_of_categories: The number of categories to classify
+        unet_levels: The number of convolutional layers for the encoder
+                     and decoder respectively.
+        number_of_start_kernels: The number of convolution kernels
+                                    to use in a convolutional layer
+        kernel_shape: The size of a kernel
+        activation: The name or object for the activation function.
+        final_activation: The activation for the output layer,
+                          e.g., sigmoid or softmax for probability.
+        pooling_amount: The factor to downsample/pool by.
+        dropout_rate: The dropout rate.
+        residual: If to include a residual layer (for res-UNet) (bool)
+        kernel_initializer: How to initialize the kernel weights.
+        """
         super(BasicUNet, self).__init__(name='')
         assert unet_levels > 0, "Unet levels is less than 1"
-        assert number_of_categories > 0, "number of classes/categories less than 1"
+        assert number_of_categories > 0,
+        "number of classes/categories less than 1"
         self.unet_levels = unet_levels
         self.number_of_start_kernels = number_of_start_kernels
         self.pooling_amount = pooling_amount
@@ -149,6 +242,13 @@ class BasicUNet(tf.keras.Model):
             kernel_initializer=kernel_initializer)
 
     def call(self, input_tensor, training=False):
+        """
+        Calls the layers on the input
+        Inputs:
+        input_tensor: input image (batches, height, width, channels)
+        training: bool that is True if paramters are being trained.
+
+        """
         down_outputs = []
 
         x = self.first_layer_conv(input_tensor)
@@ -165,11 +265,22 @@ class BasicUNet(tf.keras.Model):
 
 
 class AttentionGate(tf.keras.Model):
+        """
+        The Attention Gate Layer for Attention UNet.
 
+        This design is taken from
+        Ozan et al. "Attention U-Net: Learning Where to Look for the Pancreas"
+
+        """
     def __init__(self, num_filters, pooling_amount, kernel_initializer):
         """
-        The attention gate is used in Attention U-Net, this design is taken from Ozan et al. "Attention U-Net: Learning Where to Look for the Pancreas"
+        Initilizing the layers
 
+        Input:
+        num_filters: The number of convolution kernels
+                        to use in the attention process
+        pooling_amount: The factor to downsample/pool by.
+        kernel_initializer: How to initialize the kernel weights.
         """
         super(AttentionGate, self).__init__(name='')
 
@@ -197,6 +308,9 @@ class AttentionGate(tf.keras.Model):
         self.multiply = tf.keras.layers.Multiply()
 
     def call(self, inputs):
+        """
+        Performs the attention gating process
+        """
         x = self.W_input(inputs[0])
         g = self.W_gating_signal(inputs[1])
         x = self.add([x, g])
@@ -208,8 +322,22 @@ class AttentionGate(tf.keras.Model):
 
 
 class AttentionUNet(BasicUNet):
+    """
+    An Attention UNet Convolutional Neural Network
+
+    The network will take an input image (values between 0 and 1) 
+    and perform semantic segmentation for a fixed number of classes.
+    """
 
     def __init__(self, attention_intermediate_dim=None, *args, **kwargs):
+        """
+        Initilizing the layers
+
+        Input:
+        attention_intermediate_dim: the kernel size in the attention gate
+        
+        See UNet for args and kwargs
+        """
         super(AttentionUNet, self).__init__(*args, **kwargs)
         self.attention_gates = []
         if attention_intermediate_dim is None:
@@ -226,6 +354,13 @@ class AttentionUNet(BasicUNet):
                         value=0)))
 
     def call(self, input_tensor, training=False):
+        """
+        Calls the layers on the input
+        Inputs:
+        input_tensor: input image (batches, height, width, channels)
+        training: bool that is True if paramters are being trained.
+
+        """
         down_outputs = []
 
         x = self.first_layer_conv(input_tensor)
@@ -248,17 +383,19 @@ def make_efficientnet_spine(
         levels,
         efnet_model=tensorflow.keras.applications.efficientnet.EfficientNetB4):
     """
-    Takes an efficientnet model from keras and turns it into an encoder for UNet.
+    Takes an EfficientNet model from keras and turns it into an encoder for UNet.
 
-    The end of each block is chosen by the final addtion/reduction layers in efficientnet.
+    The end of each block is chosen by the final addtion/reduction layers in EfficientNet.
 
     Inputs:
         levels: the number of unet levels to find the blocks to use as output for the encoder
         efnet_model: the function from keras that pulls the efficientnet model
     Returns:
-        encoder: a model that takes the imput image and outputs the encoded image at each level of the unet 
+        encoder: a model that takes the imput image and outputs the 
+                 encoded image at each level of the UNet 
     """
     assert levels < 6, "Too many levels for efficientnet"
+    # We are choosing the imagenet weights for transfer learning
     efnet = efnet_model(weights='imagenet',
                         include_top=False,
                         input_shape=(None, None, 3))
@@ -276,6 +413,12 @@ def make_efficientnet_spine(
 
 
 class EfficientNetUNet(tf.keras.Model):
+    """
+    An UNet Convolutional Neural Network with EfficientNet Encoding
+    
+    The network will take an input image (values between 0 and 1) 
+    and perform semantic segmentation for a fixed number of classes.
+    """
 
     def __init__(self,
                  efficientnet,
@@ -289,6 +432,25 @@ class EfficientNetUNet(tf.keras.Model):
                  dropout_rate,
                  residual,
                  kernel_initializer=tf.keras.initializers.he_normal()):
+        """
+        Initilizing the layers
+
+        Input:
+        efficientnet: The EfficientNet constructor from keras
+        number_of_categories: The number of categories to classify
+        unet_levels: The number of convolutional layers for the encoder
+                     and decoder respectively.
+        number_of_start_kernels: The number of convolution kernels
+                                    to use in a convolutional layer
+        kernel_shape: The size of a kernel
+        activation: The name or object for the activation function.
+        final_activation: The activation for the output layer,
+                          e.g., sigmoid or softmax for probability.
+        pooling_amount: The factor to downsample/pool by.
+        dropout_rate: The dropout rate.
+        residual: If to include a residual layer (for res-UNet) (bool)
+        kernel_initializer: How to initialize the kernel weights.
+        """
         super(EfficientNetUNet, self).__init__(name='')
         assert unet_levels > 0, "Unet levels is less than 1"
         assert number_of_categories > 0, "number of classes/categories less than 1"
@@ -317,6 +479,13 @@ class EfficientNetUNet(tf.keras.Model):
             kernel_initializer=kernel_initializer)
 
     def call(self, input_tensor, training=False):
+        """
+        Calls the layers on the input
+        Inputs:
+        input_tensor: input image (batches, height, width, channels)
+        training: bool that is True if paramters are being trained.
+
+        """
         down_outputs = []
 
         outputs = self.efficientnet_spine(input_tensor * 255., training)
